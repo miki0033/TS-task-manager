99bad0321d3d4cfd32fd6316287c0547ce496f2b
miki0033 <75072281+miki0033@users.noreply.github.com>
2023-10-24 17:57:41 +0200

    2410

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import "./tailwind.css";
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+import * as UserList from "/user-list";
+import * as UserForm from "/user-form";
 
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <div class="p-4 max-w-lg mx-auto">
+        <h1 class="font-semibold text-3xl mb-4">User list</h1>
+        <ul id="users-list" class="flex flex-col gap-3"></ul>
+        <hr class="my-5" />
+        <h3 class="font-semibold text-3xl mb-4">Add User</h3>
+        <div id="add-user" class="flex flex-col gap-3"></div>
+    </div>
 `;
+
+UserList.render();
+UserForm.renderAddUserForm();
